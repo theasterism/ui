5c995fe3ff7421fdeea25e75b787870a94ec4a36
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-12-16 03:02:17 -0800

    add square variant

--- packages/orphos/src/avatar.tsx
+++ packages/orphos/src/avatar.tsx
@@ -3,14 +3,21 @@
 import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 import { cn } from "./lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
 
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root> & {
+    square?: boolean;
+  }
+>(({ className, square = false, ...props }, ref) => (
   <AvatarPrimitive.Root
     ref={ref}
-    className={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", className)}
+    className={cn(
+      "relative flex h-10 w-10 shrink-0 overflow-hidden",
+      square ? "rounded-md" : "rounded-full",
+      className,
+    )}
     {...props}
   />
 ));
