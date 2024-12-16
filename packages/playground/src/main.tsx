70884b54aee920118c4b91bf74a014b9a09f7f17
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-12-16 05:58:47 -0800

    sync

--- packages/playground/src/main.tsx
+++ packages/playground/src/main.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "next-themes";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 
+import "@theanalog/orphos/styles.css";
 import { TooltipProvider } from "@theanalog/orphos/tooltip";
 
-import "./index.css";
 import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
