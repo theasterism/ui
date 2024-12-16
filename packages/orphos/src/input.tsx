51607391331f219a7b5f6dca4417801e31891dc7
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-12-16 01:56:14 -0800

    remove unused import

--- packages/orphos/src/input.tsx
+++ packages/orphos/src/input.tsx
@@ -3,7 +3,6 @@ import { Eye, EyeClosed } from "iconoir-react";
 import * as React from "react";
 import { cn } from "./lib/utils";
 import type { FieldError } from "react-hook-form";
-import { useFormField } from "./form";
 
 const inputVariants = cva(
   "flex h-9 w-full bg-background-subtle dark:bg-background rounded-md border border-element-border focus-visible:border-transparent focus-visible:ring-2 focus-visible:ring-offset-background focus-visible:ring-info-focus-ring focus-visible:outline-none px-3 py-1 text-base shadow-sm transition-all placeholder:text-foreground-subtle placeholder:text-sm disabled:cursor-not-allowed disabled:placeholder:text-foreground-subtle/50 disabled:text-foreground-subtle disabled:bg-background-element md:text-sm",
