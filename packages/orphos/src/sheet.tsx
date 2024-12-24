eb8dbf5e8276d3ef09aa8764319489d73d9f18eb
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-12-24 00:50:46 -0800

    change button variant

--- packages/orphos/src/sheet.tsx
+++ packages/orphos/src/sheet.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
-import { cva, type VariantProps } from "cva";
-import { cn } from "./lib/utils";
+import { type VariantProps, cva } from "cva";
 import { Xmark } from "iconoir-react";
+import * as React from "react";
 import { Button } from "./button";
+import { cn } from "./lib/utils";
 
 const Sheet = SheetPrimitive.Root;
 
@@ -57,7 +57,7 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
       <SheetOverlay />
       <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
         <SheetPrimitive.Close className="absolute right-6 top-6">
-          <Button variant="soft" size="icon" className="px-2.5 overflow-hidden! w-[24px]! h-[24px]! rounded-sm">
+          <Button variant="secondary" size="icon" className="px-2.5 overflow-hidden! w-[24px]! h-[24px]! rounded-sm">
             <Xmark className="h-4 w-4" />
             <span className="sr-only">Close</span>
           </Button>
