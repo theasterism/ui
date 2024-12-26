418d3891bf07674910d510f0b7407d6f8ba82878
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-12-25 17:30:54 -0800

    sync

--- packages/playground/src/components/nav-projects.tsx
+++ packages/playground/src/components/nav-projects.tsx
@@ -37,7 +37,7 @@ export function NavProjects({
       <SidebarMenu>
         {projects.map((item) => (
           <SidebarMenuItem key={item.name}>
-            <SidebarMenuButton variant="soft" asChild>
+            <SidebarMenuButton asChild>
               <a href={item.url}>
                 <item.icon />
                 <span>{item.name}</span>
