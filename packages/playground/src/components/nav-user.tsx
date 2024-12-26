f44861b5e477fa38d934475a54b3d808ecf7c248
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-12-26 06:30:19 -0800

    sync

--- packages/playground/src/components/nav-user.tsx
+++ packages/playground/src/components/nav-user.tsx
@@ -54,9 +54,8 @@ export function NavUser({
             </SidebarMenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56"
+            side="bottom"
             sideOffset={4}
           >
             <DropdownMenuLabel className="font-normal h-auto">
