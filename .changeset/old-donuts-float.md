cc18d2e61e6c21d9e89b58593b026b859ebfbe01
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-01-29 02:44:47 -0800

    change navmenu trigger styles

---
+++ .changeset/old-donuts-float.md
@@ -0,0 +1,5 @@
+---
+"@theanalog/orphos": patch
+---
+
+change navmenu trigger styles
--- packages/orphos/src/nav-menu.tsx
+++ packages/orphos/src/nav-menu.tsx
@@ -35,7 +35,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva({
-  base: "group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-background-element-hover hover:text-foreground focus:bg-background-element-hover focus:text-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-background-element data-[state=open]:bg-background-element",
+  base: "group inline-flex h-8 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-background-element-hover hover:text-foreground focus:bg-background-element-hover focus:text-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-background-element data-[state=open]:bg-background-element",
 });
 
 const NavigationMenuTrigger = React.forwardRef<
