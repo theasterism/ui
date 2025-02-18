90264452fe2c5e1539aa5a70c5d5816452c026bc
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-02-17 21:47:20 -0800

    sync

--- packages/playground/src/components/context-menu.tsx
+++ packages/playground/src/components/context-menu.tsx
@@ -17,7 +17,7 @@ import {
 export function ContextMenuDemo() {
   return (
     <ContextMenu>
-      <ContextMenuTrigger className="flex h-[150px] w-[300px] items-center justify-center rounded-md border border-dashed text-sm">
+      <ContextMenuTrigger className="flex h-[100px] w-[200px] items-center justify-center rounded-md border border-dashed text-sm">
         Right click here
       </ContextMenuTrigger>
       <ContextMenuContent className="w-64">
