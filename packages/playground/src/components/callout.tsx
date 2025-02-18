cf21ce82e58048976215b539dd4ceac52563e3a3
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-02-17 21:45:59 -0800

    sync

--- packages/playground/src/components/callout.tsx
+++ packages/playground/src/components/callout.tsx
@@ -1,17 +1,30 @@
-import { Callout } from "@theanalog/orphos/callout";
+import { CheckCircledIcon, ExclamationTriangleIcon, InfoCircledIcon } from "@radix-ui/react-icons";
+import { Callout, CalloutTitle, CalloutDescription } from "@theanalog/orphos/callout";
 import { Fragment } from "react/jsx-runtime";
 
 export function CalloutDemo() {
   return (
     <Fragment>
-      <Callout variant="default" description="You can add components to your app using the cli." />
-      <Callout
-        bordered
-        variant="warning"
-        description="With Hidden Title! This should be much longer like 50 words or more. dismissable"
-      />
-      <Callout bordered variant="critical" description="With Hidden Title!" />
-      <Callout transparent variant="success" description="With Hidden Title! dismissable" />
+      <Callout variant="default">
+        <InfoCircledIcon />
+        <CalloutTitle>The selected emails have been marked as spam.</CalloutTitle>
+        <CalloutDescription>This is an alert with icon, title and description.</CalloutDescription>
+      </Callout>
+      <Callout bordered variant="warning">
+        <ExclamationTriangleIcon />
+        <CalloutTitle>The selected emails have been marked as spam.</CalloutTitle>
+        <CalloutDescription>This is an alert with icon, title and description.</CalloutDescription>
+      </Callout>
+      <Callout bordered variant="critical">
+        <ExclamationTriangleIcon />
+        <CalloutTitle>The selected emails have been marked as spam.</CalloutTitle>
+        <CalloutDescription>This is an alert with icon, title and description.</CalloutDescription>
+      </Callout>
+      <Callout transparent variant="success">
+        <CheckCircledIcon />
+        <CalloutTitle>The selected emails have been marked as spam.</CalloutTitle>
+        <CalloutDescription>This is an alert with icon, title and description.</CalloutDescription>
+      </Callout>{" "}
     </Fragment>
   );
 }
