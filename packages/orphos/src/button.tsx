10363c090722b3fd326c7398835a34b8a45b462d
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-02-18 16:59:35 -0800

    remove button border

--- packages/orphos/src/button.tsx
+++ packages/orphos/src/button.tsx
@@ -9,11 +9,10 @@ const buttonVariants = cva({
     variant: {
       default:
         "border border-border-element bg-background-element hover:bg-background-element-hover text-foreground active:bg-background-element-active active:border-border-element-active",
-      primary:
-        "border border-foreground/35 bg-primary/80 hover:bg-primary active:bg-primary-hover text-primary-foreground",
+      primary: "bg-primary/80 hover:bg-primary active:bg-primary-hover text-primary-foreground",
       ghost: "bg-transparent hover:bg-background-element-hover active:bg-background-element-active text-foreground",
       link: "underline-offset-4 text-primary-element-foreground bg-transparent hover:underline",
-      danger: "border border-foreground/35 bg-danger/80 hover:bg-danger active:bg-danger-hover text-danger-foreground",
+      danger: "bg-danger/80 hover:bg-danger active:bg-danger-hover text-danger-foreground",
     },
     size: {
       xs: "h-[28px] text-[0.8125rem] px-3 rounded-md",
