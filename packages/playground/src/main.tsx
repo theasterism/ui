087bb39bd6ecac7a81592f5018243180d199a7c3
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-03-16 04:45:53 -0700

    update deps

--- packages/playground/src/main.tsx
+++ packages/playground/src/main.tsx
@@ -5,16 +5,13 @@ import "./index.css";
 import { TooltipProvider } from "@theanalog/orphos/tooltip";
 
 import { App } from "./App";
-import { ThemeProvider } from "next-themes";
 import { Toaster } from "@theanalog/orphos/toaster";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
-      <TooltipProvider>
-        <App />
-        <Toaster />
-      </TooltipProvider>
-    </ThemeProvider>
+    <TooltipProvider>
+      <App />
+      <Toaster />
+    </TooltipProvider>
   </StrictMode>,
 );
