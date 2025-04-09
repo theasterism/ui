c2518b1fe5f9e8bc3d0aa56c80298c3ea0bc1402
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-04-08 20:17:47 -0700

    sync

--- packages/playground/src/components/tabs.tsx
+++ packages/playground/src/components/tabs.tsx
@@ -25,46 +25,50 @@ export function TabsDemo() {
       </TabsList>
       <TabsContent value="account">
         <Card>
-          <CardHeader className="border-b-[0.5px] border-border-subtle">
+          <CardHeader className="border-b">
             <CardTitle>Account</CardTitle>
             <CardDescription>
               Make changes to your account here. Click save when you're done.
             </CardDescription>
           </CardHeader>
-          <CardContent className="space-y-2 py-3.5">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
+          <CardContent>
+            <div className="flex flex-col gap-2.5">
+              <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" defaultValue="@peduarte" />
+              </div>
             </div>
           </CardContent>
-          <CardFooter>
+          <CardFooter className="border-t">
             <Button>Save changes</Button>
           </CardFooter>
         </Card>
       </TabsContent>
       <TabsContent value="password">
         <Card>
-          <CardHeader className="border-b-[0.5px] border-border-subtle">
+          <CardHeader className="border-b">
             <CardTitle>Password</CardTitle>
             <CardDescription>
               Change your password here. After saving, you'll be logged out.
             </CardDescription>
           </CardHeader>
-          <CardContent className="space-y-2 py-3.5">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
+          <CardContent>
+            <div className="flex flex-col gap-2.5">
+              <div className="space-y-1">
+                <Label htmlFor="current">Current password</Label>
+                <Input id="current" type="password" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">New password</Label>
+                <Input id="new" type="password" />
+              </div>
             </div>
           </CardContent>
-          <CardFooter>
+          <CardFooter className="border-t">
             <Button>Save password</Button>
           </CardFooter>
         </Card>
