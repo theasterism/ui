83cdb3768d8de3c3338c2dce29ce6bad573c86ae
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-08-18 18:33:38 -0700

    sync

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -38,7 +38,7 @@ function App() {
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+            <SidebarTrigger />
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-20 overflow-auto p-6">
