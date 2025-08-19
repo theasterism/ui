82605cc2cb3fced447fae2d706a20e24fc01fc84
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-08-18 18:08:14 -0700

    sync

--- packages/playground/src/components/sidebar/app-sidebar.tsx
+++ packages/playground/src/components/sidebar/app-sidebar.tsx
@@ -76,7 +76,7 @@ const data: {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar {...props} collapsible="offcanvas" variant="inset">
+    <Sidebar {...props} collapsible="offcanvas" variant="sidebar">
       <SidebarHeader>
         <SidebarMenuButton>
           <div className="size-5 rounded-full bg-primary" /> The Analog Co.
