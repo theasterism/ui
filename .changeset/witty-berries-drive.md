e7aee3e6221d050dab566b2c02623769eed6a938
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-08-25 01:17:28 -0700

    change code typography element color

---
+++ .changeset/witty-berries-drive.md
@@ -0,0 +1,5 @@
+---
+"orphos": patch
+---
+
+change code typography element color
--- packages/orphos/src/typography.tsx
+++ packages/orphos/src/typography.tsx
@@ -18,8 +18,8 @@ export const typographyVariants = cva({
       blockquote: "mt-6 border-primary border-l-4 pl-6 italic",
       ul: "my-6 ml-4 list-disc marker:text-primary [&>li]:mt-2 [&>li]:pl-1",
       ol: "my-6 ml-4 list-decimal marker:text-primary [&>li]:mt-2 [&>li]:pl-1",
-      code: "w-fit break-words rounded-sm bg-primary-surface px-[0.3rem] py-[0.075rem] text-primary-surface-foreground text-sm [box-decoration-break:clone] before:content-(--code-marker) after:content-(--code-marker)",
-      kbd: "order inline w-fit rounded-sm bg-background-element px-1.5 py-px text-foreground/75 text-xs [box-decoration-break:clone] dark:bg-background-element-active dark:text-foreground-subtle/85",
+      code: "w-fit break-words rounded-sm bg-background-element px-[0.3rem] py-[0.075rem] text-foreground-subtle text-sm [box-decoration-break:clone] before:content-(--code-marker) after:content-(--code-marker)",
+      kbd: "order inline w-fit rounded-sm bg-background-element-hover px-1.5 py-px text-foreground text-xs [box-decoration-break:clone] dark:bg-background-element-active dark:text-foreground-subtle/85",
     },
   },
   defaultVariants: {
