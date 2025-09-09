ef17d485ecc48da779eca2156392ebb23457ee2d
Bachitter <143877686+bachiitter@users.noreply.github.com>
2025-09-09 00:04:24 -0700

    change tooltip defaults

--- packages/orphos/src/tooltip.tsx
+++ packages/orphos/src/tooltip.tsx
@@ -6,7 +6,7 @@ import { cn } from "./utils";
 
 function TooltipProvider({
   delayDuration = 250,
-  skipDelayDuration = 0,
+  skipDelayDuration = 300,
   ...props
 }: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
   return (
@@ -53,7 +53,7 @@ function TooltipContent({
         data-slot="tooltip-content"
         sideOffset={sideOffset}
         className={cn(
-          "fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 inset-ring-1 inset-ring-border-subtle z-50 w-fit origin-(--radix-tooltip-content-transform-origin) animate-in overflow-hidden text-balance bg-foreground px-2.5 py-1 text-background *:text-base data-[state=closed]:animate-out",
+          "fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 inset-ring-1 inset-ring-border-subtle z-50 w-fit origin-(--radix-tooltip-content-transform-origin) animate-in overflow-hidden text-balance bg-foreground px-2.5 py-1 text-background text-base data-[state=closed]:animate-out",
           className
         )}
         {...props}>
