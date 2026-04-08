5f975b938d69128cfc38a6bdfb7fc8d717e883b3
Bachitter <143877686+bachiitter@users.noreply.github.com>
2026-04-08 02:40:40 -0700

    change sidebar width

--- packages/orphos/src/hooks/use-mobile.tsx
+++ packages/orphos/src/hooks/use-mobile.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const MOBILE_BREAKPOINT = 1024;
+const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
