bbd76b926e1a2c02729d8e7784247ad28f3dffd8
Bachitter <143877686+bachiitter@users.noreply.github.com>
2026-04-10 05:58:34 -0700

    add base layout, setup fonts

---
+++ packages/docs/astro.config.ts
@@ -0,0 +1,40 @@
+// @ts-check
+
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  // site: "https://orphos.pages.dev",
+  integrations: [react(), sitemap()],
+  output: "static",
+  devToolbar: {
+    enabled: false,
+  },
+  trailingSlash: "never",
+  vite: {
+    // @ts-expect-error shut up!
+    plugins: [tailwindcss()],
+  },
+  server: {
+    port: 3627,
+  },
+  fonts: [
+    {
+      name: "Inter",
+      cssVariable: "--font-inter",
+      provider: fontProviders.fontsource(),
+      formats: ["woff2", "woff"],
+      weights: ["100 900"],
+    },
+    {
+      name: "JetBrains Mono",
+      cssVariable: "--font-jetbrains-mono",
+      fallbacks: ["monospace"],
+      subsets: ["latin", "latin-ext"],
+      provider: fontProviders.fontsource(),
+    },
+  ],
+});
